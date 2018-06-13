4e965e56309258c4d5f09ef7ac7fdd47b99d8326
adieli313 <34741553+adieli313@users.noreply.github.com>
2018-06-13 16:16:25 +0300

    Add files via upload

--- TestCase.hpp
+++ TestCase.hpp
@@ -26,7 +26,7 @@ TestCase check_equal(const T& a,const T& b){
         Passed++; 
     }
     else{
-        *Error << this->TestName << ": Failure in test #" << Count << " " << a << " should equal " << b << "!" << endl; 
+        *Error << this->TestName << ": Failure in test #" << Count << ": " << a << " should equal " << b << "!" << endl; 
         Failed++;
     }
     
@@ -38,7 +38,7 @@ template <typename T>
 TestCase check_different(const T& a,const T& b){
     this->Count++;
     if((T)a == (T)b){
-        *Error << this->TestName << ": Failure in test #" << Count << " The numbers are equals!" <<endl;
+        *Error << this->TestName << ": Failure in test #" << Count << ": The numbers are equals!" <<endl;
         Failed++;
     }
     else
@@ -52,7 +52,7 @@ TestCase check_output(const T& a,const string& b){
     ostringstream keep;
     keep<<a;
     if(keep.str() != b){
-        *Error << this->TestName << ": Failure in test #" << Count << " string value should be " << b 
+        *Error << this->TestName << ": Failure in test #" << Count << ": string value should be " << b 
                   << " but is " << a << endl;
         Failed++;
         
@@ -68,7 +68,7 @@ TestCase check_function(G func,const T& Argument, const R& ExpectedResault ){
     this->Count++;
     R test = func(Argument);
     if(((R)test) != (R)ExpectedResault){
-         *Error << this->TestName << ": Failure in test #" << Count << " Function should return " << ExpectedResault 
+         *Error << this->TestName << ": Failure in test #" << Count << ": Function should return " << ExpectedResault 
                << " but returned " <<(R)test <<"!" <<endl;
         Failed++;
         
